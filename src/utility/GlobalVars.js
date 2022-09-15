c6ce3d0138853e62d108965eb1f5c77d9053cc5e
Alex Forest <azforest44@gmail.com>
2022-09-15 08:38:42 -0700

    verion test

--- src/utility/GlobalVars.js
+++ src/utility/GlobalVars.js
@@ -1,7 +1,7 @@
 const colors = {
     //primary: "rgb(21,96,137)"
-    //primary: "red"
-    primary: "#e83e8c",
+    primary: "red",
+    //primary: "#e83e8c",
     secondary: "rgba(131,147,150, 0.9)",
     //primary: "rgba(210,54,129, 0.7)"
     //primary: "rgb(154,56,110)"
